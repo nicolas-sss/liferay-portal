2b802b0707d971671949c4b9254b72c44cacb0fd
Michael Cavalcanti <michael.cavalcanti@liferay.com>
2023-07-20 17:22:06 -0300

    LPS-190521 Create test for JavaUpgradeFetchCPDefinitionByCProductExternalReferenceCodeCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -116,6 +116,15 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradeJavaFetchCPDefinitionByCProductExternalReferenceCodeCheck()
+		throws Exception {
+
+		test(
+			"upgrade/UpgradeJavaFetchCPDefinitionByCProductExternal" +
+				"ReferenceCodeCheck.testjava");
+	}
+
+	@Test
 	public void testUpgradeJavaGetFileMethodCheck() throws Exception {
 		test("upgrade/UpgradeJavaGetFileMethodCheck.testjava");
 	}
