8d4c3d8b6fd2ab6cf1e48fed505de1be107c4301
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-08-28 15:17:25 -0300

    LPS-194534 Create testUpgradeSASSCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -295,6 +295,11 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradeSASSCheck() throws Exception {
+		test("upgrade/UpgradeSASSCheck.testsass");
+	}
+
+	@Test
 	public void testUpgradeSetResultsSetTotalMethodCheck() throws Exception {
 		test("upgrade/UpgradeJavaSetResultsSetTotalMethodCheck.testjava");
 		test("upgrade/UpgradeJSPSetResultsSetTotalMethodCheck.testjsp");
