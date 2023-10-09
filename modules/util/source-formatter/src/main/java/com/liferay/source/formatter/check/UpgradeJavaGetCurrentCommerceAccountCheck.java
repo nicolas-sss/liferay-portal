73c49420b444a81d9465561ed748b93e7043b21b
Albert Gomes <albert.gomes@liferay.com>
2023-10-09 09:51:29 -0300

    LPS-196638 Create the UpgradeJavaGetCurrentCommerceAccountCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeJavaGetCurrentCommerceAccountCheck.java
@@ -0,0 +1,87 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.check.util.JavaSourceUtil;
+import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaClassParser;
+import com.liferay.source.formatter.parser.JavaMethod;
+import com.liferay.source.formatter.parser.JavaTerm;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class UpgradeJavaGetCurrentCommerceAccountCheck
+	extends BaseUpgradeCheck {
+
+	@Override
+	protected String format(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		String newContent = content;
+
+		JavaClass javaClass = JavaClassParser.parseJavaClass(fileName, content);
+
+		for (JavaTerm childJavaTerm : javaClass.getChildJavaTerms()) {
+			if (!childJavaTerm.isJavaMethod()) {
+				continue;
+			}
+
+			JavaMethod javaMethod = (JavaMethod)childJavaTerm;
+
+			String javaMethodContent = javaMethod.getContent();
+
+			Matcher matcher = _getCurrentCommerceAccountMethodPattern.matcher(
+				javaMethodContent);
+
+			while (matcher.find()) {
+				String methodCall = JavaSourceUtil.getMethodCall(
+					javaMethodContent, matcher.start());
+
+				if (!hasClassOrVariableName(
+						"CommerceAccountHelper", content, javaMethodContent,
+						methodCall)) {
+
+					continue;
+				}
+
+				List<String> parameterList = JavaSourceUtil.getParameterList(
+					methodCall);
+
+				if (parameterList.size() != 1) {
+					continue;
+				}
+
+				String[] parameterTypes = {"HttpServletRequest"};
+
+				if (!hasValidParameters(
+						1, fileName, javaMethodContent, null, parameterList,
+						parameterTypes)) {
+
+					continue;
+				}
+
+				newContent = StringUtil.replace(
+					content, methodCall,
+					StringUtil.replace(
+						methodCall, matcher.group(1),
+						"getCurrentAccountEntry(0, "));
+			}
+		}
+
+		return newContent;
+	}
+
+	private static final Pattern _getCurrentCommerceAccountMethodPattern =
+		Pattern.compile("\\w*\\.(getCurrentCommerceAccount\\()");
+
+}
