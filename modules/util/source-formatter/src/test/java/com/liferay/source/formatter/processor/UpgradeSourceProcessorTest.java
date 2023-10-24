a94dd848ea74f041670f8fd4f1b3af2c22ef0106
Micaelle Silva <micaelle.silva@liferay.com>
2023-10-24 15:19:43 -0300

    LPS-199602 Delete JavaUpgradeFetchCPDefinitionByCProductExternalReferenceCodeCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -226,15 +226,6 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
-	public void testUpgradeJavaFetchCPDefinitionByCProductExternalReferenceCodeCheck()
-		throws Exception {
-
-		test(
-			"upgrade/UpgradeJavaFetchCPDefinitionByCProductExternal" +
-				"ReferenceCodeCheck.testjava");
-	}
-
-	@Test
 	public void testUpgradeJavaGetFDSTableSchemaParameterCheck()
 		throws Exception {
 
