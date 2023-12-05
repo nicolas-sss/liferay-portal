7a7b300d4b3f01cdb41fca0905aaa243c3b0a0aa
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-12-05 16:17:39 -0300

    LPS-200550 Fix in regex for better function

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeSCSSNodeSassPatternsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeSCSSNodeSassPatternsCheck.java
@@ -98,7 +98,7 @@ public class UpgradeSCSSNodeSassPatternsCheck extends BaseUpgradeCheck {
 	}
 
 	private static final Pattern _divisionPattern = Pattern.compile(
-		"(\\$\\w+|[0-9.]+)\\s*\\/\\s*(\\$\\w+|[0-9.]+)");
+		"(\\$\\w+|[0-9]+[.]*[0-9]+)\\s*\\/\\s*(\\$\\w+|[0-9]+[.]*[0-9]+)");
 	private static final Pattern _interpolationPattern = Pattern.compile(
 		"([\\w-\\.]+)\\#\\{([\\w\\.\\$\\(\\), \\&]+)" +
 			"\\}([\\w-\\.\\#\\{\\.\\$\\}]*)");
